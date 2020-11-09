3ecff4d5d02b478234987f46bd1cf6048870ebef
kapellan221089 <serg221089@gmail.com>
2020-11-09 19:58:09 +0300

    first

---
+++ config/nuxt.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    buildModules: [
+        '@nuxtjs/vuetify',
+      ],
+      plugins: [{ src: '@/plugins/socket', ssr: false }],
+}
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const {app,server} = require('./app');
+const {Nuxt, Builder}  = require('nuxt');
+const config = require('../config/nuxt.config');
+const morgan = require('morgan');
+
+const nuxt = new Nuxt(config);
+const builder = new Builder(nuxt);
+
+app.use(morgan('dev'));
+
+
+builder.build()
+  .then(() => {
+  // Рендерить каждый маршрут с Nuxt.js
+    app.use(nuxt.render)
+    // Запустить сервер
+    server.listen(3000, ()=>{
+       console.log('Server run in 3000 port');
+    });
+  });
