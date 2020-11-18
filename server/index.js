b8a610aa7df087d9a1670e06660e2a7d011e78d3
kapellan221089 <serg221089@gmail.com>
2020-11-18 20:40:35 +0300

    1

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const app = require('express')();
 const {Nuxt, Builder}  = require('nuxt');
 const config = require('../nuxt.config.js');
 
+const nuxt = new Nuxt(config);
+const builder = new Builder(nuxt);
 
-const PORT = process.env.PORT || 80
-
+const PORT = process.env.PORT || 8000
 builder.build()
   .then(() => {
   // Рендерить каждый маршрут с Nuxt.js
