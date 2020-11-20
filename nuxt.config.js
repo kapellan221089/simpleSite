4b88a452a17225049be8b59f16b8f339996df868
kapellan221089 <serg221089@gmail.com>
2020-11-20 21:21:54 +0300

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
-import colors from 'vuetify/es5/util/colors'
+const colors = require ('vuetify/es5/util/colors')
 
-export default {
+module.exports =  {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - simple',
@@ -43,13 +43,7 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          
         }
       }
     }
