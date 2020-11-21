ec7a6b802de72673e5ea2ca7b9b2d2d108581d4f
kapellan221089 <serg221089@gmail.com>
2020-11-21 12:35:04 +0300

    1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const {app,server} = require('./app');
 const {Nuxt, Builder}  = require('nuxt');
-const config = require('../config/nuxt.config');
+const config = require('../cnuxt.config');
 const morgan = require('morgan');
 
 
